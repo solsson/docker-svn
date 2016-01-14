bc7b98ad06bddd8e562994e9063f45aee6bcd6a4
solsson <Staffan Olsson>
2016-01-14 12:55:46 +0100

    A passing test, it was all about using the right docker-compose commands

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SYNCWAIT="3"
+SYNCWAIT="1"
 
 docker-compose build
 
@@ -16,35 +16,32 @@ svn import docker-compose.yml "http://localhost:30001/svn/r1/$(date +%y%m%d_%H%M
 
 echo "# Sync job is put on some schedule or replication strategy..."
 sleep $SYNCWAIT && echo "# Sync job happens ..."
-docker-compose up -d
-docker logs dockersvn_svnsync_1
+docker-compose up svnsync
 
 echo "# Master dies and loses all its data..."
 docker kill dockersvn_svnmaster_1 && docker-compose rm -f
 
 echo "# Master is resurrected, by a replication controller probably, and gets an empty repo"
-docker-compose up -d
+docker-compose up -d svnmaster
 docker exec dockersvn_svnmaster_1 repocreate r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329 -c 1
 
 sleep $SYNCWAIT && echo "# Sync job happens again on next scheduled occasion ..."
-docker-compose up -d
-docker logs dockersvn_svnsync_1
+docker-compose up svnsync
 
 echo "# Backup service dies and loses all its data..."
 docker kill dockersvn_svnbackup_1 && docker-compose rm -f
 
 echo "# Backup goes up, gets an empty repo..."
-docker-compose up -d
+docker-compose up -d svnbackup
 docker exec dockersvn_svnbackup_1 repocreate r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329 -c 1
 
 sleep $SYNCWAIT && echo "# Sync job happens again..."
-docker-compose up -d
-docker logs dockersvn_svnsync_1
+docker-compose up svnsync
 
 echo "# Despite the dying containers our data should be on the master..."
-svn info "http://localhost:30001/svn/r1" | grep "Revision"
+svn info "http://$(docker-compose port svnmaster 80)/svn/r1/" | grep "Revision"
 echo "# ... and the slave ..."
-svn info "http://localhost:30002/svn/r1" | grep "Revision"
+svn info "http://$(docker-compose port svnbackup 80)/svn/r1/" | grep "Revision"
 
 echo "# Destoying testbed..."
 docker-compose kill && docker-compose rm -f
