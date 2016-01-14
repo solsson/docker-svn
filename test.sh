546cbe2f10d95deff23701db6519d9e1ce861b34
solsson <Staffan Olsson>
2016-01-14 08:36:30 +0100

    Runs init for a complete survival flow

--- test.sh
+++ test.sh
@@ -6,7 +6,20 @@ docker exec dockersvn_svnmaster_1 repocreate r1 -o daemon -u 3818ff11-c409-4f9e-
 docker exec dockersvn_svnbackup_1 repocreate r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329 -c 1
 
 svn import docker-compose.yml "http://localhost:30001/svn/r1/$(date +%y%m%d_%H%M%S%N).txt" -m "test"
-docker-compose up -d
-docker logs dockersvn_svnsync_1
+docker-compose up -d && docker logs dockersvn_svnsync_1
+
+# this will leave only the backup repo running
+docker kill dockersvn_svnmaster_1 && docker-compose rm -f
+docker-compose up -d && docker logs dockersvn_svnsync_1
+
+docker exec dockersvn_svnmaster_1 repocreate r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329 -c 1
+docker-compose up -d && docker logs dockersvn_svnsync_1
+
+# this will leave only the master running
+docker kill dockersvn_svnbackup_1 && docker-compose rm -f
+docker-compose up -d && docker logs dockersvn_svnsync_1
+
+docker exec dockersvn_svnslave_1 repocreate r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329 -c 1
+docker-compose up -d && docker logs dockersvn_svnsync_1
 
 # docker-compose kill && docker-compose rm -f
