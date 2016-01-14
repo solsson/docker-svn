f25b691aadc340099ce069688a3df9dae46eb2ad
solsson <Staffan Olsson>
2016-01-14 07:59:09 +0100

    Sometimes commands replace entrypoint, sometimes they append arguments, let's print informative error

--- test.sh
+++ test.sh
@@ -2,5 +2,11 @@
 docker-compose build
 docker-compose up -d
 
-docker exec dockersvn_svnmaster_1 repocreate r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329
-docker exec dockersvn_svnbackup_1 repocreate r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329
+docker exec dockersvn_svnmaster_1 repocreate r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329 -c 1
+docker exec dockersvn_svnbackup_1 repocreate r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329 -c 1
+
+svn import docker-compose.yml "http://localhost:30001/svn/r1/$(date +%y%m%d_%H%M%S%N).txt" -m "test"
+docker-compose up -d
+docker logs dockersvn_svnsync_1
+
+# docker-compose kill && docker-compose rm -f
