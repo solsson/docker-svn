39f35a1f2fb4b52287499baee2e8d89ecb8284d7
solsson <Staffan Olsson>
2016-01-14 14:15:34 +0100

    Uses docker-compose where possible

--- test.sh
+++ test.sh
@@ -2,14 +2,22 @@
 
 SYNCWAIT="1"
 
+# Failed to execute custom commands with docker-compose so we need to know container name for exec
+# This might be a REST service some day
+repocreate_svnmaster="docker exec dockersvn_svnmaster_1 repocreate"
+repocreate_svnbackup="docker exec dockersvn_svnbackup_1 repocreate"
+
 docker-compose build
 
 echo "# First start of testbed..."
 docker-compose up -d
 
+echo "# There were no repositories to sync on start..."
+docker logs dockersvn_svnsync_1
+
 echo "# Creating UUID-matching repositories on master and slave..."
-docker exec dockersvn_svnmaster_1 repocreate r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329 -c 1
-docker exec dockersvn_svnbackup_1 repocreate r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329 -c 1
+$repocreate_svnmaster r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329 -c 1
+$repocreate_svnbackup r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329 -c 1
 
 echo "# Someone commits something to master..."
 svn import docker-compose.yml "http://localhost:30001/svn/r1/$(date +%y%m%d_%H%M%S%N).txt" -m "test"
@@ -19,21 +27,21 @@ sleep $SYNCWAIT && echo "# Sync job happens ..."
 docker-compose up svnsync
 
 echo "# Master dies and loses all its data..."
-docker kill dockersvn_svnmaster_1 && docker-compose rm -f
+docker-compose kill svnmaster && docker-compose rm -f
 
 echo "# Master is resurrected, by a replication controller probably, and gets an empty repo"
 docker-compose up -d svnmaster
-docker exec dockersvn_svnmaster_1 repocreate r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329 -c 1
+$repocreate_svnmaster r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329 -c 1
 
 sleep $SYNCWAIT && echo "# Sync job happens again on next scheduled occasion ..."
 docker-compose up svnsync
 
 echo "# Backup service dies and loses all its data..."
-docker kill dockersvn_svnbackup_1 && docker-compose rm -f
+docker-compose kill svnbackup && docker-compose rm -f
 
 echo "# Backup goes up, gets an empty repo..."
 docker-compose up -d svnbackup
-docker exec dockersvn_svnbackup_1 repocreate r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329 -c 1
+$repocreate_svnbackup r1 -o daemon -u 3818ff11-c409-4f9e-acd1-1d9903fc4329 -c 1
 
 sleep $SYNCWAIT && echo "# Sync job happens again..."
 docker-compose up svnsync
