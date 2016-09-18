f5e0364dbc6c793652836772ab0e3f1b5baa71a8
Staffan Olsson <staffan@repos.se>
2016-09-18 06:14:05 +0200

    Just run build-contract, verifies that we have a permission problem for repoadmin REST

--- build-contracts/test1/test1.sh
+++ build-contracts/test1/test1.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-RETRY="--retry 3 --retry-delay 10"
+[ -z "$RETRY" ] & RETRY="--retry 3 --retry-delay 5"
 
-curl $RETRY -f http://svn/svn/ || exit 1
+curl $RETRY -f http://svn/svn/ -I || exit 1
 
 curl $RETRY -f http://svn/admin/repocreate -d reponame=test1 -d owner=daemon || exit 1
 
+curl $RETRY -f http://svn/svn/test1/ -I || exit 1
+
+echo "Really successful? Results from the cgi script are very unreliable. See logs."
 echo "During development the test script will hang here for 1 hour"
 sleep 3600
