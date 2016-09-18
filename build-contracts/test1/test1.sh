cf84daec86a982828fa233044666cb762b9556dc
Staffan Olsson <staffan@repos.se>
2016-09-18 19:18:39 +0200

    REST repo creation will always result in ownership that allows commits over apache, i.e. daemon user

--- build-contracts/test1/test1.sh
+++ build-contracts/test1/test1.sh
@@ -3,7 +3,7 @@
 
 curl $RETRY -f http://svn/svn/ -I || exit 1
 
-curl $RETRY -f http://svn/admin/repocreate -d reponame=test1 -d owner=daemon || exit 1
+curl $RETRY -f http://svn/admin/repocreate -d reponame=test1 || exit 1
 
 curl $RETRY -f http://svn/svn/test1/ -I || exit 1
 
