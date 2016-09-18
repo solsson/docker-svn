d269ea11a3a12dd0805dc55dcb9973507b3e180a
Staffan Olsson <staffan@repos.se>
2016-09-18 14:57:07 +0200

    Blocks repocreate unless the flag env var is set

--- build-contracts/test1/test1.sh
+++ build-contracts/test1/test1.sh
@@ -7,6 +7,12 @@ curl $RETRY -f http://svn/admin/repocreate -d reponame=test1 -d owner=daemon || 
 
 curl $RETRY -f http://svn/svn/test1/ -I || exit 1
 
-echo "Really successful? Results from the cgi script are very unreliable. See logs."
+#noaccess=$(curl $RETRY -f http://svn_noadminrest/admin/repocreate -I)
+#echo $noaccess
+#echo $noaccess | grep -q "403" || exit 2
+# Currently we get 500 from all errors, which causes retries
+curl http://svn_noadminrest/admin/repocreate -I | grep -q "500" || exit 1
+
+echo "Really successful? Response codes from the cgi script are very uninformative. See logs until die->500 is fixed, at least."
 echo "During development the test script will hang here for 1 hour"
 sleep 3600
